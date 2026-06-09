63533fad5f6d6857cf927d6c84e41c62f8b154f1
Agustin Berge <agustinberge@gmail.com>
2026-06-09 10:44:06 +0200

    [ci] Test cmake integration

---
+++ test/cmake-fetch_content/main.cpp
@@ -0,0 +1,13 @@
+// Eggs.Test
+//
+// Copyright Agustin K-ballo Berge, Fusion Fenix 2026
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <eggs/test.hpp>
+
+TEST_CASE(verify, "verifies the library links and runs")
+{
+    CHECK(1 + 1 == 2);
+}
